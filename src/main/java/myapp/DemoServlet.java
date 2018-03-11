ac0ae3e13a128c7a1ba9a0f67827182903d68ca7
swethakannan7 <swetha.kannan7@gmail.com>
2018-03-11 15:16:01 +0530

    Update DemoServlet.java

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -38,19 +38,6 @@ public class DemoServlet extends HttpServlet {
       throws IOException {
     resp.setContentType("text/plain");
     resp.getWriter().println("{ \"name\": \"Swethakannan\" }");
-    hitCount++; 
-      PrintWriter out = resp.getWriter();
-      String title = "Total Number of Hits";
-      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
-      
-      out.println(docType +
-         "<html>\n" +
-            "<head><title>" + title + "</title></head>\n" +
-            "<body bgcolor = \"#f0f0f0\">\n" +
-               "<h1 align = \"center\">" + title + "</h1>\n" +
-               "<h2 align = \"center\">" + hitCount + "</h2>\n" +
-            "</body>"+
-         "</html>"
-      );
+ 
   }
 }
