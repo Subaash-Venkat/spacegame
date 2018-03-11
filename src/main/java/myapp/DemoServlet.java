948d48ab56a166b3e7bbe62c18a6ae0ab769820a
swethakannan7 <swetha.kannan7@gmail.com>
2018-03-11 14:57:38 +0530

    Update DemoServlet.java

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -22,6 +22,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DemoServlet extends HttpServlet {
+  
+  private int hitCount; 
+
+   public void init() { 
+      // Reset hit counter.
+      hitCount = 0;
+   } 
+  hitCount++; 
+      PrintWriter out = response.getWriter();
+      String title = "Total Number of Hits";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+      
+      out.println(docType +
+         "<html>\n" +
+            "<head><title>" + title + "</title></head>\n" +
+            "<body bgcolor = \"#f0f0f0\">\n" +
+               "<h1 align = \"center\">" + title + "</h1>\n" +
+               "<h2 align = \"center\">" + hitCount + "</h2>\n" +
+            "</body>
+         </html>"
+      );
+  
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
